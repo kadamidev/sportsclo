e068d390f4dbae0dc6d7426821b60cbb21ba83e6
Dev <kadamidev@users.noreply.github.com>
2022-01-23 21:20:25 +0700

    mobile grid layout complete

--- components/ItemGrid/ItemGrid.jsx
+++ components/ItemGrid/ItemGrid.jsx
@@ -1,24 +1,49 @@
 import Image from "next/image"
 import styles from "./ItemGrid.module.scss"
 import { items } from "../../utils/data"
-import React from "react"
+import React, { useState, useRef } from "react"
 
 export default function ItemGrid() {
+  const filtersRef = useRef()
+  const [filter, setFilter] = useState(0)
+  const [showFilterSelection, setShowFilterSelection] = useState(false)
+
+  const filterOptions = [
+    "price: low-to-high",
+    "price: high-to-low",
+    "newest-to-oldest",
+  ]
+
   return (
     <section className={styles.itemGridContainer}>
       <div className={styles.top}>
         <div className={styles.filterWrapper}>
           <Image src="/filter.svg" width={18} height={18} />
         </div>
-        <div className={styles.sortContainer}>
+        <div
+          className={styles.sortContainer}
+          onClick={() => setShowFilterSelection(!showFilterSelection)}
+        >
           <div className={styles.sortSvgWrapper}>
             <Image src="/sort.svg" width={20} height={16} />
           </div>
-          <span className={styles.sortType}>price: low-to-high</span>
-          {/* <select name="sort">
-            <option value="price-l-h">price: low-to-high</option>
-            <option value="price-h-l">price: high-to-low</option>
-          </select> */}
+          <span className={styles.sortType}>{filterOptions[filter]}</span>
+          {showFilterSelection && (
+            <div ref={filtersRef} className={styles.filterTypesPopup}>
+              <ul>
+                {filterOptions.map((filter, idx) => (
+                  <li
+                    onClick={() => {
+                      setFilter(idx)
+                    }}
+                    key={idx}
+                  >
+                    {filter}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
       <ul className={styles.itemList}>
@@ -31,17 +56,18 @@ export default function ItemGrid() {
                   alt={item.image.alt}
                   width={200}
                   height={200}
-                  // layout="intrinsic"
+                  layout="responsive"
                   objectFit="cover"
                 />
               </div>
               <div className={styles.itemDetails}>
-                <span>{item.name}</span>
-                <span>&#36;{item.price}.00</span>
+                <span className={styles.itemName}>{item.name}</span>
+                <span className={styles.itemPrice}>&#36;{item.price}.00</span>
               </div>
             </li>
           )
         })}
+        <li className={styles.ghostElm}></li>
       </ul>
     </section>
   )
